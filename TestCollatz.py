45ff474de05911a7a5e80ddbfa59376aff83ffe0
justwinata <justwinata@gmail.com>
2015-06-16 11:50:34 -0500

    Adding unit tests

--- TestCollatz.py
+++ TestCollatz.py
@@ -26,16 +26,27 @@ class TestCollatz (TestCase) :
     # read
     # ----
 
-    def test_read (self) :
+    def test_read_1 (self) :
         s    = "1 10\n"
         i, j = collatz_read(s)
         self.assertEqual(i,  1)
         self.assertEqual(j, 10)
 
+    def test_read_2 (self) :
+        s = "2 20 more\n"
+        i, j = collatz_read(s)
+        self.assertEqual(i,  2)
+        self.assertEqual(j, 20)
+
+    def test_read_3 (self) :
+        s = "10 1"
+        i, j = collatz_read(s)
+        self.assertEqual(i, null)
+
     # ----
     # eval
     # ----
-
+'''
     def test_eval_1 (self) :
         v = collatz_eval(1, 10)
         self.assertEqual(v, 20)
@@ -51,26 +62,40 @@ class TestCollatz (TestCase) :
     def test_eval_4 (self) :
         v = collatz_eval(900, 1000)
         self.assertEqual(v, 174)
-
+'''
     # -----
     # print
     # -----
 
-    def test_print (self) :
+    def test_print_1 (self) :
         w = StringIO()
         collatz_print(w, 1, 10, 20)
         self.assertEqual(w.getvalue(), "1 10 20\n")
 
+    def test_print_2 (self) :
+        w = StringIO()
+        collatz_print(w, 100, 200, 125)
+        self.assertEqual(w.getvalue(), "100 200 125\n")
+
+    def test_print_3 (self) :
+        w = StringIO()
+        collatz_print(w, 201, 210, 89)
+        self.assertEqual(w.getvalue(), "201 210 89\n")
+
+    def test_print_4 (self) :
+        w = StringIO()
+        collatz_print(w, 900, 1000, 174)
+        self.assertEqual(w.getvalue(), "900 1000 174\n")
     # -----
     # solve
     # -----
-
+'''
     def test_solve (self) :
         r = StringIO("1 10\n100 200\n201 210\n900 1000\n")
         w = StringIO()
         collatz_solve(r, w)
-        self.assertEqual(w.getvalue(), "1 10 1\n100 200 1\n201 210 1\n900 1000 1\n")
-
+        self.assertEqual(w.getvalue(), "1 10 20\n100 200 125\n201 210 89\n900 1000 174\n")
+'''
 # ----
 # main
 # ----
